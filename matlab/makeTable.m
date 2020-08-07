eaff80d3f921556663c1f19681839cfe4997336d
KevDuc <kduconge@laposte.net>
2020-08-07 07:42:39 +0200

    MATLAB code to create table

--- matlab/makeTable.m
+++ matlab/makeTable.m
@@ -1,10 +1,25 @@
-dataFolder = "../data";
-files = dir(dataFolder + "/*.json");
-files = dataFolder + "/" + string({files.name});
-
-for i = 1:size(files,2)
-    file = files(i);
-    text = fileread(file);
-    json = jsondecode(text);
-    disp(json);
+function [tableTotal, tableTotalStacked] = makeTable(dataFile)
+if nargin == 0
+    dataFile = "../data-compiled.json";
 end
+
+% t = table('Size', [0 4], 'VariableTypes', ["string","string","string","string"], 'VariableNames', ["arabic", "arabicIDs", "english", "englishIDs"]);
+
+text = fileread(dataFile);
+data = jsondecode(text);
+data.arabic = string(data.arabic);
+data.english = string(data.english);
+% data.translationNum = "english"+string(data.translationNum);
+
+tableTotalStacked = sortrows(unique(struct2table(data)), "arabic");
+
+g = findgroups(tableTotalStacked.arabic);
+d = [0; diff(g) == 0];
+for i = 2:size(d,1)
+    if (d(i) > 0)
+        d(i) = d(i-1) + d(i);
+    end
+end
+
+tableTotal = [tableTotalStacked, table("english"+d, 'VariableNames', "translationNum")];
+tableTotal = unstack(tableTotal, "english", "translationNum");
